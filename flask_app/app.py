0769b76f3961ea2a466d52e0ec36fa9a7de22aae
Rani <rani.linkov@spot.io>
2021-03-22 21:52:29 +0200

    testing html and css
    Signed-off-by: Rani <rani.linkov@spot.io>

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from requests import get, post
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('layout.html')
 
 
 if __name__ == '__main__':
