0c447db161e6c4abbb7c68cca9cd451c91eaf9e4
Yoav Amoyal <yoav@Yoavs-MacBook.local>
2021-03-23 01:07:06 +0200

    added teletext font and basic layout

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,11 +5,11 @@ from requests import get, post
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
+@app.route('/good_vibes_mainpage')
+def good_vibes_mainpage():
     return render_template('layout.html')
 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0',use_reloader=True, port=port)
+    app.run(debug=True, host='0.0.0.0', use_reloader=True, port=port)
